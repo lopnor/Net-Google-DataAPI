81a6f5e0fe34aa8ab85f4f002bde288ec7f1f21e
Nobuo Danjou <nobuo.danjou@gmail.com>
2009-08-21 08:21:29 +0900

    initial commit

---
+++ lib/Net/Google/GData.pm
@@ -0,0 +1,33 @@
+package Net::Google::GData;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Net::Google::GData -
+
+=head1 SYNOPSIS
+
+  use Net::Google::GData;
+
+=head1 DESCRIPTION
+
+Net::Google::GData is
+
+=head1 AUTHOR
+
+Nobuo Danjou E<lt>nobuo.danjou@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
