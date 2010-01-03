ffa4f990a4fc06dd00b3460903c2f8928cb406a6
Nobuo Danjou <nobuo.danjou@gmail.com>
2010-01-04 04:58:48 +0900

    Checking in changes prior to tagging of version 0.12.  Changelog diff is:
    diff --git a/Changes b/Changes
    index 8c68076..2bb1970 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,10 @@
     Revision history for Perl extension Net::Google::GData
    
    +0.12    Mon Jan 04 04:21:00 2010
    +
    +        - new Net::Google::DataAPI::Auth::ClientLogin::Multiple
    +        - new prepare_request method
    +
     0.11    Thu Dec 31 18:30:00 2009
    
             - refactored internals

--- lib/Net/Google/DataAPI.pm
+++ lib/Net/Google/DataAPI.pm
@@ -5,7 +5,7 @@ use Any::Moose '::Exporter';
 use Carp;
 use Lingua::EN::Inflect::Number qw(to_PL);
 use XML::Atom;
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 
 any_moose('::Exporter')->setup_import_methods(
     as_is => ['feedurl', 'entry_has'],
---
+++ lib/Net/Google/DataAPI/Auth/ClientLogin/Multiple.pm
@@ -0,0 +1,88 @@
+package Net::Google::DataAPI::Auth::ClientLogin::Multiple;
+use Any::Moose;
+use Net::Google::AuthSub;
+with 'Net::Google::DataAPI::Role::Auth';
+
+our $VERSION = '0.01';
+
+has account_type => ( is => 'ro', isa => 'Str', required => 1, default => 'HOSTED_OR_GOOGLE' );
+has source   => ( is => 'ro', isa => 'Str', required => 1, default => __PACKAGE__ );
+has username => ( is => 'ro', isa => 'Str', required => 1 );
+has password => ( is => 'ro', isa => 'Str', required => 1 );
+has services => ( is => 'ro', isa => 'HashRef', required => 1 );
+has tokens => ( is => 'rw', isa => 'HashRef', default => sub { +{} });
+
+sub sign_request {
+    my ($self, $req) = @_;
+    my $host = $req->uri->host;
+    $self->tokens->{$host} ||= $self->_get_auth_params($host);
+    $req->header(@{$self->tokens->{$host}});
+    return $req;
+}
+
+sub _get_auth_params {
+    my ($self, $host) = @_;
+    exists $self->services->{$host} 
+        or confess "service for $host not defined";
+    my $authsub = Net::Google::AuthSub->new(
+        source      => $self->source,
+        service     => $self->services->{$host},
+        accountType => $self->account_type,
+    );
+    my $res = $authsub->login(
+        $self->username,
+        $self->password,
+    );
+    $res->is_success or confess "login for $host failed";
+    return [ $authsub->auth_params ];
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Net::Google::DataAPI::Auth::ClientLogin::Multiple - keeps and sings auth_params for multiple Google Data API domains
+
+=head1 SYNOPSIS
+
+  use Net::Google::DataAPI::Auth::ClientLogin::Multiple;
+
+  my $auth = Net::Google::DataAPI::Auth::ClientLogin::Multiple->new(
+    username => 'foo.bar@gmail.com',
+    password => 'p4ssw0rd',
+    services => {
+        'docs.google.com' => 'writely',
+        'spreadsheets.google.com' => 'wise',
+    }
+  );
+  my $req = HTTP::Request->new(
+    'GET' => 'https://docs.google.com/feeds/default/private/full'
+  );
+  $auth->sign_request($req);
+  # sets $req Authorization header
+
+=head1 DESCRIPTION
+
+This module keeps and sings auth_params for multiple google Data API domains.
+
+=head1 AUTHOR
+
+Nobuo Danjou E<lt>nobuo.danjou@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+L<Net::Google::AuthSub>
+
+L<Net::Google::DataAPI>
+
+L<http://code.google.com/intl/en/apis/accounts/docs/AuthForInstalledApps.html>
+
+L<http://code.google.com/intl/en/apis/gdata/faq.html#clientlogin>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
