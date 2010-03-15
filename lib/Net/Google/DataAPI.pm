4e917809272b47e81e1eb96bccb3b85c6a2b56fe
Nobuo Danjou <nobuo.danjou@gmail.com>
2010-03-15 22:51:38 +0900

    Checking in changes prior to tagging of version 0.17.  Changelog diff is:
    diff --git a/Changes b/Changes
    index 0608ca4..efe65ef 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension Net::Google::GData
    
    +0.17    Sat Mar 15 22:51:00 2010
    +
    +        - depends on Mouse 0.51
    +
     0.16    Sat Jan 16 18:05:00 2010
    
             - depends on Net::Google::AuthSub 0.5

--- lib/Net/Google/DataAPI.pm
+++ lib/Net/Google/DataAPI.pm
@@ -5,7 +5,7 @@ use Any::Moose '::Exporter';
 use Carp;
 use Lingua::EN::Inflect::Number qw(to_PL);
 use XML::Atom;
-our $VERSION = '0.16';
+our $VERSION = '0.17';
 
 any_moose('::Exporter')->setup_import_methods(
     as_is => ['feedurl', 'entry_has'],
