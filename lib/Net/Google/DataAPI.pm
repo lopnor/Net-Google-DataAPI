0aa43123bc0ea2ddeef051fdd7ea51e9304de694
Nobuo Danjou <nobuo.danjou@gmail.com>
2011-06-13 15:20:58 +0900

    Checking in changes prior to tagging of version 0.24.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index f7cc63e..eecf3fc 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension Net::Google::DataAPI
    
    +0.24    Mon Jun 13 15:14:00 2011
    +        - requires LWP::Protocol::https
    +
     0.23    Sun Nov 28 19:00:00 2010
    
             - Content-Type check only work with request with response_object parameter

--- lib/Net/Google/DataAPI.pm
+++ lib/Net/Google/DataAPI.pm
@@ -5,7 +5,7 @@ use Any::Moose '::Exporter';
 use Carp;
 use Lingua::EN::Inflect::Number qw(to_PL);
 use XML::Atom;
-our $VERSION = '0.23';
+our $VERSION = '0.24';
 
 any_moose('::Exporter')->setup_import_methods(
     as_is => ['feedurl', 'entry_has'],
