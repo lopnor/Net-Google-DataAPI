1d024cb6b2c9bafb3891476c127843d084d3c468
Nobuo Danjou <danjou@soffritto.org>
2012-04-06 10:50:55 +0900

    Checking in changes prior to tagging of version 0.26_1.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 435c6ae..cad6065 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension Net::Google::DataAPI
    
    +0.26_1  Fri Apr 06 11:00:00 2012
    +        - fix test failures
    +        - requires Net::OAuth2 0.07
    +
     0.26    Thu Apr 05 20:15:00 2012
             - Supports OAuth2 with Net::Google::DataAPI::Auth::OAuth2
             - now Net::Google::DataAPI::Role::Service->request does not fail with 30X codes

--- lib/Net/Google/DataAPI.pm
+++ lib/Net/Google/DataAPI.pm
@@ -5,7 +5,7 @@ use Any::Moose '::Exporter';
 use Carp;
 use Lingua::EN::Inflect::Number qw(to_PL);
 use XML::Atom;
-our $VERSION = '0.26';
+our $VERSION = '0.26_1';
 
 any_moose('::Exporter')->setup_import_methods(
     as_is => ['feedurl', 'entry_has'],
