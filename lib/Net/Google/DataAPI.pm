83a185ef3a28d1b65eef3c64db491d99bb682329
Nobuo Danjou <danjou@soffritto.org>
2012-04-06 22:18:49 +0900

    Checking in changes prior to tagging of version 0.27.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 611b1cf..dc00973 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension Net::Google::DataAPI
    
    +0.27    Fri Apr 06 22:10:00 2012
    +        - official release (no changes except for the version number)
    +
     0.26_2  Fri Apr 06 12:55:00 2012
             - change author address

--- lib/Net/Google/DataAPI.pm
+++ lib/Net/Google/DataAPI.pm
@@ -5,7 +5,7 @@ use Any::Moose '::Exporter';
 use Carp;
 use Lingua::EN::Inflect::Number qw(to_PL);
 use XML::Atom;
-our $VERSION = '0.26_2';
+our $VERSION = '0.27';
 
 any_moose('::Exporter')->setup_import_methods(
     as_is => ['feedurl', 'entry_has'],
